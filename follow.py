5a317114eff622fc25b241f7e7e430575a6035a7
Raj Shah <brainix@gmail.com>
2014-09-10 01:48:14 -0700

    followed back

---
+++ follow.py
@@ -0,0 +1,47 @@
+#-----------------------------------------------------------------------------#
+#   Quarter Bot - A Twitter bot that flips a coin.                            #
+#   follow.py                                                                 #
+#                                                                             #
+#   Copyright (c) 2014, Seventy Four, Inc.                                    #
+#                                                                             #
+#   This program is free software: you can redistribute it and/or modify      #
+#   it under the terms of the GNU General Public License as published by      #
+#   the Free Software Foundation, either version 3 of the License, or         #
+#   (at your option) any later version.                                       #
+#                                                                             #
+#   This program is distributed in the hope that it will be useful,           #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#   GNU General Public License for more details.                              #
+#                                                                             #
+#   You should have received a copy of the GNU General Public License         #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+#-----------------------------------------------------------------------------#
+
+
+
+import tweepy
+
+import bot
+
+
+
+def get_items(*args, **kwargs):
+    cursor = tweepy.Cursor(*args, **kwargs)
+    pages = cursor.pages()
+    items = []
+    for page in pages:
+        items.extend(page)
+    items = set(items)
+    return items
+
+def main():
+    follower_ids = get_items(bot.api.followers_ids)
+    following_ids = get_items(bot.api.friends_ids)
+    diff = follower_ids - following_ids
+    for user_id in diff:
+        print('Following: {0}'.format(user_id))
+        bot.api.create_friendship(user_id=user_id, follow=False)
+
+if __name__ == '__main__':
+    main()
